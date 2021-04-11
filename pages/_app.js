98d60a4c8c1e74fbad0af4d3f05caba08744cc31
vignesh <vignesh.kc@zohocorp.com>
2021-04-11 17:34:51 +0530

    Blog post

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import fs from "fs";
-import Link from "next/link"
-import Layout from "../components/layout"
-import path from "path"
-import matter from "gray-matter"
+import React from 'react';
+import Head from 'next/head';
+import fs from 'fs';
+import Link from 'next/link';
+import path from 'path';
+import matter from 'gray-matter';
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ slugs }) {
   return (
     <>
-    <Head>
+      <Head>
         <title>Vignesh Karunakaran</title>
         <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Layout>
-    <div className={styles.container}>
+      </Head>
+      <Layout>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>Welcome to Space</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Space
-        </h1>
-
-        
-          <div>
-            {slugs.map(slug => {
-              return (
-                <div href="#" className={styles.card}>
-                  <Link href={`/blog/${encodeURIComponent(slug.href)}`}><a>
-                  <h3>{slug.title}</h3>
-                  <p>{slug.desc}</p>
-                  </a></Link>
+            <div>
+              {slugs.map((slug) => (
+                <div className={styles.card}>
+                  <Link href={`/writeup/${encodeURIComponent(slug.href)}`}>
+                    <a href>
+                      <h3>{slug.title}</h3>
+                      <p className={styles.date}>{slug.date}</p>
+                      <p>{slug.desc}</p>
+                    </a>
+                  </Link>
                 </div>
-              );
-            })}
-          </div>
-      </main>
-    </div>
-    </Layout>
+              ))}
+            </div>
+          </main>
+        </div>
+      </Layout>
     </>
-  )
+  );
 }
+
 export const getStaticProps = async () => {
-  const files = fs.readdirSync("posts").map(filename => filename.replace(".md", ""));
-  let data = [];
+  const files = fs.readdirSync('posts').map((filename) => filename.replace('.md', ''));
+  const data = [];
   files.forEach((postPath) => {
-    let dataObj = {};
-    let  markdownWithMetadata = fs.readFileSync(path.join("posts", postPath + ".md")).toString();
-    let parsedMarkdownMeta =  matter(markdownWithMetadata).data;
+    const dataObj = {};
+    const markdownWithMetadata = fs.readFileSync(path.join('posts', `${postPath}.md`)).toString();
+    const parsedMarkdownMeta = matter(markdownWithMetadata).data;
     dataObj.title = parsedMarkdownMeta.title;
     dataObj.desc = parsedMarkdownMeta.description;
+    dataObj.date = parsedMarkdownMeta.date;
     dataObj.href = postPath;
     data.push(dataObj);
   });
   return {
     props: {
-      slugs: data
-    }
+      slugs: data,
+    },
   };
 };
