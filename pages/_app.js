f9bbca9a1c36a56f23a03ceed7dbd69b58207f29
vk3yr3dw0n <official2vignesh@gmail.com>
2021-04-11 14:06:07 +0000

    Deploying to gh-pages from @ vk3yr3dw0n/my-blog@edf2ccc0768555df563c64c0f287f21ea32a0ad9 🚀

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
