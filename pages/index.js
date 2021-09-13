48b79bbd0368fe31a565118d43a64322f7902f69
vignesh karunakaran <official2vignesh@gmail.com>
2021-09-13 14:55:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,7 @@ export default function Home({ slugs }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="google-site-verification" content="Wcflcs2x61VjBAxMxXhi5tqkpT-Z5HTpTMn0CYvAsJQ" />
         <meta name="description" content="Personal blog by Vignesh Karunakaran" />
+        <meta name="author" content="Vignesh Karunakaran" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-VC6QSDP6M6"></script>
       </Head>
       <Layout>
