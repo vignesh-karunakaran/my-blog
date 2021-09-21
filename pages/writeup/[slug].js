a91eddc1a459b73a8f4cf18b745c9d1beee2c419
vignesh karunakaran <official2vignesh@gmail.com>
2021-09-21 17:51:35 +0530

    Added inspectlet heatMap on posts

--- pages/writeup/[slug].js
+++ pages/writeup/[slug].js
@@ -17,6 +17,13 @@ const Post = ({ htmlString, data, isLive, gToken }) => {
       }
       gtag('js', new Date());
       gtag('config', gToken, { page_path: window.location.pathname });
+      (function() {
+      window.__insp = window.__insp || [];
+      __insp.push(['wid', 960675955]);
+      var ldinsp = function(){
+      if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=960675955&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+      setTimeout(ldinsp, 0);
+      })();
     }
   });
  
