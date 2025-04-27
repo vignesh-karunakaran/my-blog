c6e88599a9512d9dd293f92f3a28d12bdc9642ed
vignesh karunakaran <official2vignesh@gmail.com>
2025-04-27 12:44:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home({ slugs, isLive, gToken, TelegramBotToken }) {
             <header className={styles.header}>
             <div className={styles.headerCover}></div>
             <div className={styles.headerTitleCover}>
-            <h1 className={styles.title}>Welcome to Space</h1>
+            <h1 className={styles.title}>Vignesh Karunakaran</h1>
             </div>
             </header>
 
